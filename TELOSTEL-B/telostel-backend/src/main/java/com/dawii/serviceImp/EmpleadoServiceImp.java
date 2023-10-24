f089c93809e53be8903cd72faac7fa78145532bc
luisxd0812 <xScreamorex@hotmail.com>
2023-10-24 13:18:49 -0500

    etiqueta service en empleado

--- TELOSTEL-B/telostel-backend/src/main/java/com/dawii/serviceImp/EmpleadoServiceImp.java
+++ TELOSTEL-B/telostel-backend/src/main/java/com/dawii/serviceImp/EmpleadoServiceImp.java
@@ -2,10 +2,12 @@ package com.dawii.serviceImp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import com.dawii.dao.IEmpleadoDAO;
 import com.dawii.entity.Empleado;
 
+@Service
 public class EmpleadoServiceImp extends ICRUDImpl<Empleado, Long> {
 
 	@Autowired
