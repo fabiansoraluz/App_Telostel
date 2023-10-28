2ccc661c0f03f4e6ebdfff0161a54d683082546f
luisxd0812 <xScreamorex@hotmail.com>
2023-10-28 01:19:26 -0500

    Correccion del error en EmpleadoController - Backend

--- TELOSTEL-B/telostel-backend/src/main/java/com/dawii/service/EmpleadoService.java
+++ TELOSTEL-B/telostel-backend/src/main/java/com/dawii/service/EmpleadoService.java
@@ -1,12 +1,10 @@
 package com.dawii.service;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dawii.dao.IEmpleadoDAO;
-import com.dawii.entity.Empleado;
 
 @Service
 public class EmpleadoService {
@@ -22,18 +20,12 @@ public class EmpleadoService {
 		return repo.existsByCelular(celular);
 	}
 	
-	//CRUD
-	public Empleado grabar(Empleado bean) {
-		return repo.save(bean);
-	}
-	public Empleado buscarXId(Long id) {
-		return repo.findById(id).orElse(null);
-	}
-	public List<Empleado> listarEmpleados(){
-		return repo.findAll();
-	}
-	public void eliminar(Long id) {
-		repo.deleteById(id);
-	}
+	public boolean existeOtroXDni(Long id, String dni) {
+        return repo.existsByDniAndIdNot(dni, id);
+    }
+
+    public boolean existeOtroXCelular(Long id, String celular) {
+        return repo.existsByCelularAndIdNot(celular, id);
+    }
 	
 }
